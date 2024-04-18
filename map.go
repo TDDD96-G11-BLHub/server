818928ea073e04ab20b22f8f92a8694ece3deb12
Jacob <jacalz@tutanota.com>
2024-04-18 14:14:01 +0200

    Tidy up support for storing marker stuff

--- map.go
+++ map.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log/slog"
 	"net/http"
 	"strconv"
@@ -30,7 +29,8 @@ type mapHandler struct {
 }
 
 func (s *mapHandler) addSensorData(c *gin.Context) {
-	form := &sensorData{ID: s.id.Add(1)}
+	id := s.id.Add(1) - 1 // Increment directly but decrement so we get zero-indexing.
+	form := &sensorData{ID: id}
 
 	if err := c.ShouldBindJSON(form); err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
@@ -38,26 +38,61 @@ func (s *mapHandler) addSensorData(c *gin.Context) {
 		return
 	}
 
-	fmt.Println(form)
-
 	s.mu.Lock()
 	s.data = append(s.data, form)
 	s.mu.Unlock()
+
+	slog.Info("Stored a new sensor data", "id", form.ID, "timestamp", form.Date)
 }
 
 func (s *mapHandler) getMapCoordinates(c *gin.Context) {
-	fmt.Println("FETCH COORDINATES")
+	type coordinateData struct {
+		ID        uint64  `json:"id"`
+		Latitude  float64 `json:"lat"`
+		Longitude float64 `json:"lng"`
+	}
 
 	s.mu.RLock()
-	c.JSON(http.StatusOK, s.data)
+	markers := make([]coordinateData, len(s.data))
+	for i, data := range s.data {
+		markers[i] = coordinateData{
+			ID:        data.ID,
+			Latitude:  data.Latitude,
+			Longitude: data.Longitude,
+		}
+	}
 	s.mu.RUnlock()
+
+	/*
+		// Fake coordinates data for the map:
+		markers = []coordinateData{
+			{ID: 1, Latitude: 30.1695, Longitude: 10.9354},
+			{ID: 2, Latitude: 61.1695, Longitude: 24.9354},
+			{ID: 3, Latitude: 63.1695, Longitude: 24.9354},
+			{ID: 4, Latitude: 64.1695, Longitude: 24.9354},
+			{ID: 5, Latitude: 65.1695, Longitude: 24.9354},
+		}
+	*/
+
+	c.JSON(http.StatusOK, gin.H{"markers": markers})
+
+	slog.Info("Sent over all map coordinates")
 }
 
 func (s *mapHandler) getMarker(c *gin.Context) {
-	id, _ := strconv.Atoi(c.Param("markerID"))
-	fmt.Println("Marker ID: ", id)
+	param := c.Param("markerID")
+	id, err := strconv.Atoi(param)
+	if err != nil {
+		c.JSON(http.StatusNotFound, "Incorrect marker ID")
+		slog.Error("Could not parse marker ID", slog.String("id", param))
+		return
+	}
 
-	// fake sensor types data with unit
+	s.mu.RLock()
+	data := s.data[id]
+	s.mu.RUnlock()
+
+	// Fake sensor types data with unit:
 	sensorTypes := []map[string]string{
 		{"type": "temperature", "unit": "°C"},
 		{"type": "humidity", "unit": "%"},
@@ -65,23 +100,13 @@ func (s *mapHandler) getMarker(c *gin.Context) {
 		{"type": "moisture", "unit": "%"},
 	}
 
-	// current time
-	currentTime := time.Now().Format(time.DateTime)
-
-	// Include a custom message in the response
 	c.JSON(http.StatusOK, gin.H{
 		"sensorID":    id,
-		"sensorName":  "SENSOR EXAMPLE NAME",
+		"sensorName":  data.Type + "sensor",
 		"sensorImage": "https://www.plantagen.se/dw/image/v2/BCMR_PRD/on/demandware.static/-/Library-Sites-PlantagenShared/default/dw258d02d2/1000/elefantore-pilea-peperomioides.jpg?sw=1024",
 		"sensorTypes": sensorTypes,
-		"lastUpdated": currentTime,
+		"lastUpdated": data.Date.Format(time.DateTime),
 	})
 
-	/*
-		s.mu.RLock()
-		data := s.data[id]
-		s.mu.RUnlock()
-
-		c.JSON(http.StatusOK, data)
-	*/
+	slog.Info("Sent over data for a marker", "id", data.ID, "timestamp", data.Date)
 }
